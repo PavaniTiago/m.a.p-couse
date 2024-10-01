8c3fffc10d852e1c771ffa0c61d985918f01cab0
Pavani Tiago <tiagopavanisalto@hotmail.com>
2024-10-01 18:05:13 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
-    }
+    },
+    basePath: '/curso-completo',  // Define o caminho base para a URL
+    assetPrefix: 'https://alessandrapavani.com/curso-completo/',  // Define o prefixo de recursos para os assets
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
